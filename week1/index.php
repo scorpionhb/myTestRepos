0912fe41f98cde8315d48aff1caa2e4250ea92d4
ACER <dasda>
2016-10-03 23:57:58 +0100

    functions

--- week1/index.php
+++ week1/index.php
@@ -102,14 +102,14 @@
                 }
 
                 function wanted($name, $specs, $mugs, $rolls){
-                    $var = 5;
+                    $var = "5";
                     echo "Wanted: " . $name . "<br>";
                     echo "Known to be in possession of the following items:" . "<br>";
                     echo "Specs: (" . $specs . ")<br>";
                     echo "Mugs: (" . $mugs . ")<br>";
                     echo "Sausage rolls (" . $rolls . ")<br>";
                     echo "Award for capture: $";
-                    $pls =  award($specs, $mugs, $rolls) + $var ;
+                    $pls = award($specs, $mugs, $rolls) + $var ;
                     print $pls;
                 }
 
