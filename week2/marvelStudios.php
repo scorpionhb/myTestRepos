537e8ff76b16e42681f963da0af8a3d320e52cce
ACER <dasda>
2016-10-10 23:15:11 +0100

    marvel try

--- week2/marvelStudios.php
+++ week2/marvelStudios.php
@@ -7,4 +7,33 @@
  */
 ?>
 
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>All Movies</title>
+    </head>
+
+    <body>
+
+    <?php
+    //Including the database for further usage
+    include("dbConnect.php");
+
+    //The SQL query to show all movies by Marvel Studios
+    $sql_query = "SELECT * FROM marvelmovies WHERE productionStudio IS 'Marvel Studios'";
+
+    //Collecting the result of the query
+    $result = $db->query($sql_query);
+
+    //Processing the result
+    while($row = $result->fetch_array()){
+        echo "<p> $row[yearReleased] -- $row[title] -- $row[productionStudio] -- $row[notes]</p>";
+        echo "<p> </p>";
+    }
+
+    ?>
+
+    </body>
+
+</html>
 
