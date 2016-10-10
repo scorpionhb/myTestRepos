32038627cf4339af896a6b21fe6069aadb61b188
ACER <dasda>
2016-10-10 23:27:22 +0100

    after 2010 try

--- week2/after2010.php
+++ week2/after2010.php
@@ -7,4 +7,35 @@
  */
 ?>
 
+<!DOCTYPE html>
+<html>
 
+    <head>
+        <title>After 2010</title>
+    </head>
+
+    <body>
+
+    <?php
+
+    //Including the database
+    include("dbConnect.php");
+
+    //Creating the query that is required for this page
+    $myQuery = "SELECT * FROM marvelmovies WHERE yearRelease > '2010'";
+
+    //Collecting the query's result
+    $result = $db->query($myQuery);
+
+    //Displaying the results
+    while($row = $result->fetch_array()){
+        echo "<p>$row[yearReleased] -- $row[title] -- $row[productionStudio] -- $row[notes]</p>";
+        echo "<p> </p>";
+    }
+
+    ?>
+
+
+    </body>
+
+</html>
