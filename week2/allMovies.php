f046f934ca7db659aa6aebc3a9eecd53c8808f65
ACER <dasda>
2016-10-10 22:27:46 +0100

    allMovies

---
+++ week2/allMovies.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ACER
+ * Date: 10/10/2016
+ * Time: 22:26
+ */
---
+++ week2/indexPage.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ACER
+ * Date: 10/10/2016
+ * Time: 22:21
+ */
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>Index Page</title>
+    </head>
+
+    <body>
+
+        <header>This is the index page for the databases lab</header>
+
+        <p>If you want to see the list of ALL MOVIES click the link bellow:
+            <a href="allMovies.php">All Movies</a>
+        </p>
+
+
+
+
+    </body>
+
+</html>
