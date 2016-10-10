e1e9010665d7f81f41e27aa2ef14cd9b984b0f33
ACER <dasda>
2016-10-10 23:32:10 +0100

    x-men try

--- week2/xmen.php
+++ week2/xmen.php
@@ -6,4 +6,35 @@
  * Time: 22:38
  */
 ?>
+<!DOCTYPE html>
+<html>
 
+    <head>
+        <title>X-Men</title>
+    </head>
+
+    <body>
+
+    <?php
+
+        //Including the database
+        include("dbConnect.php");
+
+        //Creating the query that is required for this page
+        $myQuery = "SELECT * FROM marvelmovies WHERE title LIKE '%X-men%'";
+
+        //Collecting the query's result
+        $result = $db->query($myQuery);
+
+        //Displaying the results
+        while($row = $result->fetch_array()){
+            echo "<p>$row[yearReleased] -- $row[title] -- $row[productionStudio] -- $row[notes]</p>";
+            echo "<p> </p>";
+        }
+
+    ?>
+
+
+    </body>
+
+</html>
