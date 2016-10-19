890be4a65158527f88d9672148aa97bc6de0b772
ACER <dasda>
2016-10-19 21:19:54 +0100

    with get

---
+++ week4/superheroForm.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * User: ACER
+ * Date: 19/10/2016
+ */
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Form</title>
+    </head>
+
+    <body>
+        <form action="superPrint.php" method="post">
+            <label>First name:</label><input type="text" name="firstName">
+            <label>Last name:</label><input type="text" name="lastName">
+            <input type="radio" name="gender" value="male">Male<br>
+            <input type="radio" name="gender" value="female">Female<br>
+        </form>
+    </body>
+
+</html>
+
