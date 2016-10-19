be54c11d86c4115b2372fc9b2339f0efc05a1dab
ACER <dasda>
2016-10-19 22:21:05 +0100

    post method

--- week4/superPrint.php
+++ week4/superPrint.php
@@ -19,7 +19,7 @@
             echo "Superhero details:<br>";
             echo "$firstName " . " $lastName " . " - $gender <br> " . "Power: $power<br>";
         ?>
-        <img src="https://s-media-cache-ak0.pinimg.com/originals/ba/75/d9/ba75d933e2d5186ecdd8d2049ca978c9.jpg"/>
+        <img src="https://dncache-mauganscorp.netdna-ssl.com/thumbseg/1106/1106096-bigthumbnail.jpg"/>
     </body>
 
 </html>
