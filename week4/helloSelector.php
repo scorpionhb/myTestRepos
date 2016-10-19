910b5d740f419e6992879bb260d0e5f013a8e352
ACER <dasda>
2016-10-19 19:35:51 +0100

    planets

---
+++ week4/helloSelector.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ACER
+ * Date: 19/10/2016
+ * Time: 19:19
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Selector</title>
+    </head>
+
+    <body>
+        <h1>Where would you like to say hello?</h1>
+        <a href="">Earth</a>
+        <p>Mars</p>
+        <p>Uranus</p>
+    </body>
+
+
+
+
+
+</html>
