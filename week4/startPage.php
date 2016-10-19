4782b11d23ff91ed2e10f5810d540b6e35c5fec5
ACER <dasda>
2016-10-19 22:24:09 +0100

    post method

---
+++ week4/startPage.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * User: ACER
+ * Date: 19/10/2016
+ */
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Starting Page</title>
+    </head>
+    <body>
+        <h2>Select the exercise that you want to check:</h2>
+        <a href="helloSelector.php">Ex.1 (Hello Selector)</a>
+    </body>
+
+</html>
