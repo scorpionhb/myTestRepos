eff59896874ba77e5b93d4bf4f4dbfc8fa7d8c36
ACER <dasda>
2016-10-19 19:47:40 +0100

    with get

--- week4/helloSelector.php
+++ week4/helloSelector.php
@@ -15,9 +15,9 @@
 
     <body>
         <h1>Where would you like to say hello?</h1>
-        <a href="http://testapphristo.azurewebsites.net/week4/helloPrinter.php?query=earth">Earth</a>
-        <a href="http://testapphristo.azurewebsites.net/week4/helloPrinter.php?query=mars">Mars</a>
-        <a href="http://testapphristo.azurewebsites.net/week4/helloPrinter.php?query=uranus">Uranus</a>
+        <a href="http://testapphristo.azurewebsites.net/week4/helloPrinter.php?query=Earth">Earth</a>
+        <a href="http://testapphristo.azurewebsites.net/week4/helloPrinter.php?query=Mars">Mars</a>
+        <a href="http://testapphristo.azurewebsites.net/week4/helloPrinter.php?query=Uranus">Uranus</a>
     </body>
 
 
