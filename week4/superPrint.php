5b1582eb74431582f89efc57e55d12784c284a88
ACER <dasda>
2016-10-19 22:15:53 +0100

    post method

--- week4/superPrint.php
+++ week4/superPrint.php
@@ -17,7 +17,7 @@
             $gender = $_POST["gender"];
             $power = $_POST["power"];
             echo "Superhero details:<br>";
-            echo "$firstName " . " $lastName " . " - $gender <br> " . "Power: $power";
+            echo "$firstName " . " $lastName " . " - $gender <br> " . "Power: $power<br>";
         ?>
         <img src="https://s-media-cache-ak0.pinimg.com/originals/ba/75/d9/ba75d933e2d5186ecdd8d2049ca978c9.jpg"/>
     </body>
