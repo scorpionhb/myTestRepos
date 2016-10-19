f160f8969f68e39ec8f38efb17ed7858bd556bd2
ACER <dasda>
2016-10-19 23:37:09 +0100

    db create

--- week4/insertSuperhero.php
+++ week4/insertSuperhero.php
@@ -14,36 +14,7 @@
         <?php
             include("dbConnect.php");
 
-    /**
-        $servername = "localhost";
-        $username = "username";
-        $password = "password";
-        $dbname = "myDB";
-
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-*/
-        // sql to create table
-        $sql = "CREATE TABLE 'superheroes'(
-              'superheroID' int(10) NOT NULL AUTO_INCREMENT,
-              'firstName' text NOT NULL,
-              'lastName' text NOT NULL,
-              'superPower' text NOT NULL,
-              PRIMARY KEY('superheroID')
-              )";
-
-        if ($db->query($sql) === TRUE) {
-            echo "Table MyGuests created successfully";
-        } else {
-            echo "Error creating table: " . $db->error;
-        }
-
-        $db->close();
-
+    
         ?>
 
     </body>
