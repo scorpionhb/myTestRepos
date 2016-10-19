f9fcf5600723f7f05d3d128664809518379b8beb
ACER <dasda>
2016-10-19 22:51:01 +0100

    post method

---
+++ week4/insertSuperhero.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * User: ACER
+ * Date: 19/10/2016
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Insert superhero</title>
+    </head>
+
+    <body>
+        <?php
+            include("dbConnect.php");
+        ?>
+
+    </body>
+
+</html>
