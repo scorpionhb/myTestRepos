b06b697aafdd9df17323189dbbbe40640d3e0ae3
ACER <dasda>
2016-10-24 14:31:12 +0100

    username/pass

---
+++ week5/checkLogin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ACER
+ * Date: 24/10/2016
+ * Time: 14:28
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Check Login</title>
+    </head>
+
+    <body>
+        <p>Hi!</p>
+
+    </body>
+
+
+</html>
---
+++ week5/index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ACER
+ * Date: 24/10/2016
+ * Time: 14:19
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Index page</title>
+    </head>
+
+    <body>
+        <form action="checkLogin.php" method="post">
+            <p>Name:</p><br>
+            <input type="text" name="name">
+            <p>Passoword:</p>
+            <input type="password" name="password"><br>
+            <input type="submit" name="Log In">
+        </form>
+    </body>
+
+
+</html>
