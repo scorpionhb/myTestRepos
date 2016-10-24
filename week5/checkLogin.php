cbc075628c0d81238908c07db6d80916e1f16d04
ACER <dasda>
2016-10-24 15:19:39 +0100

    tesst

--- week5/checkLogin.php
+++ week5/checkLogin.php
@@ -19,7 +19,7 @@
             $password = $_POST["password"];
 
             if($username == "username" && $password == "password"){
-                setcookie('access_level_cookie', 'root');
+                setcookie('access_level_cookie', 'standarduser');
             }
             header('Location: loggedIn.php');
 
