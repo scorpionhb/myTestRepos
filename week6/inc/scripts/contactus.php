485dd4630167cdb06d3790909be74dc1a0fa1c7a
scorpionhb <hdx@abv.bg>
2016-10-31 15:00:25 +0000

    week6

---
+++ week6/inc/scripts/contactus.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1405564
+ * Date: 31/10/2016
+ * Time: 14:58
+ */
+
+include ("scripts/tinymce/header.php");
+
+echo "
+<main>
+<h2>Contact</h2>
+<p>Send me an email or call me</p>
+";
+
+incude ("scripts/tinymce/footer.php");
+ 
+?>
