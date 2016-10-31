72377ed6bfe9f2668560d835654965d68b7a6005
scorpionhb <hdx@abv.bg>
2016-10-31 15:52:18 +0000

    week6

--- week6/index.php
+++ week6/index.php
@@ -1,42 +1,43 @@
-<h1>hello</h1>
-<?php
-/**
- * Created by PhpStorm.
- * User: 1405564
- * Date: 31/10/2016
- * Time: 14:08
- */
+<?
 
 define('INCLUDE_DIR', dirname(__FILE__) . '/inc/');
 
 $rules = array(
+    //
     //main pages
+    //
     'about' => "/about",
     'contactus' => "/contactus",
     'blog' => "/blog",
     'blog_article' => "/blog/(?'blogID'[\w\-]+)",
 
-    //admin pages
+    //
+    //Admin Pages
+    //
     'login' => "/login",
     'create_article' => "/createarticle",
     'logout' => "/logout",
-
-    //home page
+    //
+    // Home Page
+    //
     'home' => "/"
+    //
+    // Style
+    //
 );
 
-$uri = rtrim(dirname($_SERVER["SCRIPT_NAME"], '/'));
+$uri = rtrim(dirname($_SERVER["SCRIPT_NAME"]), '/');
 $uri = '/' . trim(str_replace($uri, '', $_SERVER['REQUEST_URI']), '/');
 $uri = urldecode($uri);
 
-foreach ($rules as $action => $rules){
-    if (preg_match('~^' . $rule . '$~i', $uri, $param)) {
+foreach ($rules as $action => $rule) {
+    if (preg_match('~^' . $rule . '$~i', $uri, $params)) {
         include(INCLUDE_DIR . $action . '.php');
         exit();
     }
 }
 
-//404 error
+// nothing is found so handle the 404 error
 include(INCLUDE_DIR . '404.php');
 
 ?>
