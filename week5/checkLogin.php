5d3dedcc85d44d8b028bf15e5035a4630e74a648
ACER <dasda>
2016-12-12 12:05:46 +0000

    tesst

--- week5/checkLogin.php
+++ week5/checkLogin.php
@@ -20,8 +20,9 @@
 
             if($username == "username" && $password == "password"){
                 setcookie('access_level_cookie', 'standarduser');
+                header('Location: loggedIn.php');
             }
-            header('Location: loggedIn.php');
+
 
         ?>
 
